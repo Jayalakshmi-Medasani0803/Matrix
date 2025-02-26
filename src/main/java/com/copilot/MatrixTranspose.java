0399f4cc3a0d4c5048c564e28ecfb8854d11f6b2
jayamedasani <jayalakshmi.medasani@wavemaker.com>
2025-02-26 15:39:30 +0530

    Added transpose of matrix

--- src/main/java/com/copilot/MatrixTranspose.java
+++ src/main/java/com/copilot/MatrixTranspose.java
@@ -5,12 +5,27 @@ public class MatrixTranspose {
         int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         int rows = matrix.length;
         int columns = matrix[0].length;
+        //in case of printing
         for (int j = 0; j < columns; j++) {
             for (int i = 0; i < rows; i++) {
                 System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
         }
-
+        //change actual matrix
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        //print matrix
+        for (int j = 0; j < rows; j++) {
+            for (int i = 0; i < columns; i++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
